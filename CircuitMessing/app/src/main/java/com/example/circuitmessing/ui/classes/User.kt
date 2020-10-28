d709ea939aa6ec981a53f5ac402ddcd0b1973567
Bruno Žitković <brunozitkovic@gmail.com>
2020-10-28 23:46:45 +0100

    Added login and registration implementation with the database

---
+++ CircuitMessing/app/src/main/java/com/example/circuitmessing/ui/classes/User.kt
@@ -0,0 +1,7 @@
+package com.example.circuitmessing.ui.classes
+
+public class User(username: String, password: String, points: Int) {
+    val username: String = username
+    val password: String = password
+    var points: Int = points
+}
