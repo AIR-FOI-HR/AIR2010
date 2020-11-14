bc1bd9fcea082de488026fb18332e22ff467bec9
Antonio <antoniomax98@gmail.com>
2020-11-14 23:09:30 +0100

    Implemented item drawers in product views

--- CircuitMessing/app/src/main/java/com/example/circuitmessing/products/ringo/ActivityRingo.kt
+++ CircuitMessing/app/src/main/java/com/example/circuitmessing/products/ringo/ActivityRingo.kt
@@ -3,13 +3,29 @@ package com.example.circuitmessing.products.ringo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.circuitmessing.MainActivity
 import com.example.circuitmessing.R
+import com.google.android.material.navigation.NavigationView
 
 class ActivityRingo : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private var mFragmentManager: FragmentManager = supportFragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ringo)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
     }
 
     override fun onBackPressed() {
