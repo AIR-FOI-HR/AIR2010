338480e90931c85088af8a27aff289b4525cb54d
Bruno Žitković <brunozitkovic@gmail.com>
2020-11-16 20:16:51 +0100

    added selected page highlight and current user username in drawer header. Also added username in preferences.

--- CircuitMessing/app/src/main/java/com/example/circuitmessing/products/nibble/ActivityNibble.kt
+++ CircuitMessing/app/src/main/java/com/example/circuitmessing/products/nibble/ActivityNibble.kt
@@ -3,8 +3,11 @@ package com.example.circuitmessing.products.nibble
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
+import com.escaper.escaper.utils.preferences
 import com.example.circuitmessing.MainActivity
 import com.example.circuitmessing.R
 import com.google.android.material.navigation.NavigationView
@@ -18,11 +21,34 @@ class ActivityNibble : AppCompatActivity() {
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
+
+        // Setting navHeader text to the current user username
+        val headerView = navView.getHeaderView(0)
+        val navUsername = headerView.findViewById<View>(R.id.username) as TextView
+        navUsername.text = preferences.username
+
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_home -> {
+                    returnHome()
+                }
+                // All other cases for drawer items will go here also
+            }
+
+            // set item as selected to persist highlight
+            menuItem.isChecked = true
+            // close drawer when item is tapped
+            drawerLayout.closeDrawers()
+            true
+        }
     }
 
     override fun onBackPressed() {
         super.onBackPressed()
+        returnHome()
+    }
 
+    private fun returnHome() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
--- CircuitMessing/app/src/main/java/com/example/circuitmessing/products/ringo/ActivityRingo.kt
+++ CircuitMessing/app/src/main/java/com/example/circuitmessing/products/ringo/ActivityRingo.kt
@@ -1,18 +1,21 @@
 package com.example.circuitmessing.products.ringo
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.FragmentManager
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import com.escaper.escaper.utils.preferences
 import com.example.circuitmessing.MainActivity
 import com.example.circuitmessing.R
+import com.example.circuitmessing.databinding.ActivityRingoBinding
 import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.nav_header_ringo.view.*
+
 
 class ActivityRingo : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -26,13 +29,39 @@ class ActivityRingo : AppCompatActivity() {
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
+
+        // Setting navHeader text to the current user username
+        val headerView = navView.getHeaderView(0)
+        val navUsername = headerView.findViewById<View>(R.id.username) as TextView
+        navUsername.text = preferences.username
+
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_home -> {
+                    returnHome()
+                }
+
+                // All other cases for drawer items will go here also
+            }
+
+            // set item as selected to persist highlight
+            menuItem.isChecked = true
+            // close drawer when item is tapped
+            drawerLayout.closeDrawers()
+            true
+        }
     }
 
     override fun onBackPressed() {
         super.onBackPressed()
+        returnHome()
 
+    }
+
+    private fun returnHome() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
     }
 }
+
