c9d8634a6e5b3a42c5502ee3bf21b1d710dc3c1d
unknown <jvresk@foi.hr>
2020-12-21 19:20:19 +0100

    added layout for multiple choice quiz questions

---
+++ CircuitMessing/app/src/main/java/com/example/circuitmessing/products/quiz/views/MultipleChoiceQuestionFragment.kt
@@ -0,0 +1,32 @@
+package com.example.circuitmessing.products.quiz.views
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.circuitmessing.R
+
+class MultipleChoiceQuestionFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MultipleChoiceQuestionFragment()
+    }
+
+    private lateinit var viewModel: MultipleChoiceQuestionViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.multiple_choice_question_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MultipleChoiceQuestionViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ CircuitMessing/app/src/main/java/com/example/circuitmessing/products/quiz/views/MultipleChoiceQuestionViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.circuitmessing.products.quiz.views
+
+import androidx.lifecycle.ViewModel
+
+class MultipleChoiceQuestionViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
